063e0ae5ee732a7344f56ab61e4fdcf27143c7cb
Federico Burgardt <federico@woowup.com>
2019-09-17 13:27:27 -0300

    Debugger de products

--- src/Stages/Products/WoowUpProductDebugger.php
+++ src/Stages/Products/WoowUpProductDebugger.php
@@ -9,15 +9,7 @@ class WoowUpProductDebugger extends DebugUploadStage
 	public function __invoke($payload)
 	{
 		foreach ($payload as $p) {
-			if (strpos($p['sku'], "1193260")) {
-				var_dump([
-					'sku' => $p['sku'],
-					'name' => $p['name'],
-					'base_name' => $p['base_name'],
-					'stock' => $p['stock'],
-					'available' => $p['available'],
-				]);
-			}
+			var_dump($p);
 		}
 
 		return [];
