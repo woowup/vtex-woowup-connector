dca9117eacf226bdf5e981a1262d5a0fbea1f80b
kevin mendoza <kevinnnnmarcelo@gmail.com>
2021-08-19 13:05:21 -0300

    verificamos que el code exista en el cliente cuando falla la subida

--- src/Stages/Customers/WoowUpCustomerUploader.php
+++ src/Stages/Customers/WoowUpCustomerUploader.php
@@ -2,6 +2,7 @@
 
 namespace WoowUpConnectors\Stages\Customers;
 
+use GuzzleHttp\Exception\RequestException;
 use League\Pipeline\StageInterface;
 
 class WoowUpCustomerUploader implements StageInterface
@@ -42,23 +43,48 @@ class WoowUpCustomerUploader implements StageInterface
                 $this->logger->info("[Customer] " . implode(',', $customerIdentity) . " Updated Successfully");
                 $this->woowupStats['updated']++;
             }
-        } catch (\Exception $e) {
-            if (method_exists($e, 'getResponse')) {
-                $response     = json_decode($e->getResponse()->getBody(), true);
-                $errorCode    = $response['code'];
-                $errorMessage = $response['payload']['errors'][0] ?? json_encode($response);
-            } else {
-                $errorCode    = $e->getCode();
-                $errorMessage = $e->getMessage();
+        } catch (RequestException $e) {
+            $errorCode    = $e->getCode();
+            $errorMessage = $e->getMessage();
+            if ($e->hasResponse()) {
+                $response = $e->getResponse();
+                $errorInformation = $this->getErrorInformation($response, $errorCode, $errorMessage);
+                $errorCode = $errorInformation['code'];
+                $errorMessage = $errorInformation['message'];
             }
             $this->logger->info("[Customer] " . implode(',', $customerIdentity) . " Error:  Code '" . $errorCode . "', Message '" . $errorMessage . "'");
             $this->woowupStats['failed'][] = $customer;
-
+            return false;
+        } catch (\Exception $e) {
+            $errorCode    = $e->getCode();
+            $errorMessage = $e->getMessage();
+            $this->logger->info("[Customer] " . implode(',', $customerIdentity) . " Error:  Code '" . $errorCode . "', Message '" . $errorMessage . "'");
+            $this->woowupStats['failed'][] = $customer;
             return false;
         }
 
         return true;
 	}
+
+    protected function getErrorInformation ($response, $errorCode, $errorMessage) {
+        $errorInformation = [
+            'code' => $errorCode,
+            'message' => $errorMessage
+        ];
+        $body = json_decode((string) $response->getBody(),true);
+        if  ($body) {
+            if (isset($body['code']) && !empty($body['code'])) {
+                $errorInformation['code'] = $body['code'];
+                if ($body['code'] == 'internal_error') {
+                    $errorInformation['message'] = $body['message'] ?? '';
+                } else {
+                    $errors = $body['payload']['errors'] ?? [];
+                    $errorInformation['message'] = implode(';',$errors);
+                }
+            }
+        }
+        return $errorInformation;
+    }
 
 	public function getWoowupStats()
 	{
