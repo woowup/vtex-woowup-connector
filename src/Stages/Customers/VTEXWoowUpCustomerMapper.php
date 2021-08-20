28b93a9aa455ac98daa9a0beb11849a5e0ceae0e
Joaquin Beracochea <joaquin@woowup.com>
2021-08-20 12:25:08 -0300

    agregamos cambios para obtener apiKey y modificamos la logica del opt-in

--- src/Stages/Customers/VTEXWoowUpCustomerMapper.php
+++ src/Stages/Customers/VTEXWoowUpCustomerMapper.php
@@ -2,46 +2,51 @@
 
 namespace WoowUpConnectors\Stages\Customers;
 
+use Exception;
+use GuzzleHttp\ClientInterface;
 use League\Pipeline\StageInterface;
+use WoowUpConnectors\Exceptions\VTEXRequestException;
 
 class VTEXWoowUpCustomerMapper implements StageInterface
 {
-    const COMMUNICATION_ENABLED  = 'enabled';
+    const COMMUNICATION_ENABLED = 'enabled';
     const COMMUNICATION_DISABLED = 'disabled';
-    const DISABLED_REASON_OTHER  = 'other';
-    const INVALID_EMAILS         = ['ct.vtex.com.br', 'mercadolibre.com'];
+    const DISABLED_REASON_OTHER = 'other';
+    const INVALID_EMAILS = ['ct.vtex.com.br', 'mercadolibre.com'];
 
-	protected $vtexConnector;
+    protected $vtexConnector;
     protected $logger;
+    private $apiKey;
+    private ClientInterface $_httpClient;
 
-	public function __construct($vtexConnector, $logger)
-	{
-		$this->vtexConnector = $vtexConnector;
-        $this->logger        = $logger;
-
-        return $this;
-	}
-
-	public function __invoke($payload)
-	{
-		if (is_null($payload)) {
-			return null;
-		}
-
-		return $this->buildCustomer($payload);
-	}
-
-	protected function buildCustomer($vtexCustomer)
+    public function __construct($vtexConnector, $logger,$apiKey)
     {
-        $email    = isset($vtexCustomer->email) && !empty($vtexCustomer->email) ? $vtexCustomer->email : null;
+        $this->vtexConnector = $vtexConnector;
+        $this->logger = $logger;
+        $this->apiKey = $apiKey;
+        return $this;
+    }
+
+    public function __invoke($payload)
+    {
+        if (is_null($payload)) {
+            return null;
+        }
+
+        return $this->buildCustomer($payload);
+    }
+
+    protected function buildCustomer($vtexCustomer)
+    {
+        $email = isset($vtexCustomer->email) && !empty($vtexCustomer->email) ? $vtexCustomer->email : null;
         $document = isset($vtexCustomer->document) && !empty($vtexCustomer->document) ? $vtexCustomer->document : null;
 
         if (!empty($email) || !empty($document)) {
             $customer = [
-                'email'         => $email,
-                'document'      => $document,
-                'first_name'    => ucwords(mb_strtolower($vtexCustomer->firstName)),
-                'last_name'     => ucwords(mb_strtolower($vtexCustomer->lastName)),
+                'email' => $email,
+                'document' => $document,
+                'first_name' => ucwords(mb_strtolower($vtexCustomer->firstName)),
+                'last_name' => ucwords(mb_strtolower($vtexCustomer->lastName)),
             ];
 
             if (isset($vtexCustomer->birthDate) && !empty($vtexCustomer->birthDate)) {
@@ -50,15 +55,57 @@ class VTEXWoowUpCustomerMapper implements StageInterface
             }
 
             if (isset($vtexCustomer->homePhone) && !empty($vtexCustomer->homePhone)) {
-            	$customer['phone'] = $vtexCustomer->homePhone;
+                $customer['phone'] = $vtexCustomer->homePhone;
             }
 
             if (isset($vtexCustomer->documentType) && !empty($vtexCustomer->documentType)) {
-            	$customer['document_type'] = $vtexCustomer->documentType;
+                $customer['document_type'] = $vtexCustomer->documentType;
+            }
+            $endpoint = 'https://api.woowup.com/apiv3/users';
+            $queryParams = [];
+            if (isset($email) && !empty($email)) {
+                $queryParams['email'] = $email;
+            }
+            if (isset($document) && !empty($document)) {
+                $queryParams['document'] = $document;
             }
 
-            if (isset($customer['mailing_enabled_reason']) && ($customer['mailing_enabled_reason'] != 'other'))
-            {
+            try {
+                $response = $this->_httpClient->request('GET', $endpoint, [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'Accept' => 'application/json',
+                        'Authorization' => 'Basic ' . $this->apiKey,
+                    ],
+                    'query' => $queryParams,
+                ]);
+
+                if (in_array($response->getStatusCode(), [200, 206])) {
+                    $body = $response->getBody();
+                }
+            } catch (Exception $e) {
+                if (method_exists($e, 'getResponse') &&
+                    method_exists($e->getResponse(), 'getStatusCode') &&
+                    method_exists($e->getResponse(), 'getBody')) {
+                    $response = $e->getResponse();
+                    $code = $response->getStatusCode();
+                    $body = (string)$response->getBody();
+                    $body = json_decode($body);
+                    $message = $body->Message ?? $code;
+                    $this->logger->error("Error [" . $code . "] " . $message);
+                    if ($response->getStatusCode() >= 400 && $response->getStatusCode() < 500) {
+                        throw new VTEXRequestException($message, $code, $endpoint, $queryParams);
+                    } else {
+                        throw new VTEXRequestException($message, $code, $endpoint, $queryParams, true);
+                    }
+                } else {
+                    $this->logger->error("Error at request attempt " . $e->getMessage());
+                }
+            }
+            $mailing_enabled_reason = $body->payload->mailing_enabled_reason;
+
+
+            if($mailing_enabled_reason !== 'other') {
                 if (isset($vtexCustomer->isNewsletterOptIn)) {
                     if (!$vtexCustomer->isNewsletterOptIn) {
                         $customer['mailing_enabled'] = self::COMMUNICATION_DISABLED;
@@ -71,12 +118,13 @@ class VTEXWoowUpCustomerMapper implements StageInterface
                     }
                 }
             }
-            
+
+
             if (isset($customer['email'])) {
                 foreach (self::INVALID_EMAILS as $email) {
                     if (stripos($customer['email'], $email) !== false) {
-                        $customer['email']                  = $customer['document'].'@noemail.com';
-                        $customer['mailing_enabled']        = self::COMMUNICATION_DISABLED;
+                        $customer['email'] = $customer['document'] . '@noemail.com';
+                        $customer['mailing_enabled'] = self::COMMUNICATION_DISABLED;
                         $customer['mailing_enabled_reason'] = self::DISABLED_REASON_OTHER;
                     }
                 }
@@ -84,23 +132,23 @@ class VTEXWoowUpCustomerMapper implements StageInterface
 
             if (isset($vtexCustomer->isNewsletterOptIn)) {
                 if (!$vtexCustomer->isNewsletterOptIn) {
-                    $customer['custom_attributes']      = [
-                        'opt_in_vtex'  => 'False',
+                    $customer['custom_attributes'] = [
+                        'opt_in_vtex' => 'False',
                     ];
                 } else {
-                    $customer['custom_attributes']      = [
-                        'opt_in_vtex'  => 'True',
+                    $customer['custom_attributes'] = [
+                        'opt_in_vtex' => 'True',
                     ];
                 }
             }
-            
+
             try {
                 $vtexAddress = $this->vtexConnector->getAddress($vtexCustomer->id);
                 if (isset($vtexAddress)) {
-                	$address = $this->buildAddress($vtexAddress);
+                    $address = $this->buildAddress($vtexAddress);
                     $customer += $address;
                 }
-            } catch (\Exception $e) {
+            } catch (Exception $e) {
                 $this->logger->info("Error getting address: " . $e->getMessage());
             }
 
@@ -118,15 +166,15 @@ class VTEXWoowUpCustomerMapper implements StageInterface
 
     protected function buildAddress($vtexAddress)
     {
-        $street  = ucwords(mb_strtolower($vtexAddress->street));
+        $street = ucwords(mb_strtolower($vtexAddress->street));
         $street .= isset($vtexAddress->number) ? (' ' . $vtexAddress->number) : '';
 
         $address = [
-            'street'   => $street,
+            'street' => $street,
             'postcode' => $vtexAddress->postalCode,
-            'city'     => ucwords(mb_strtolower($vtexAddress->city)),
-            'state'    => ucwords(mb_strtolower($vtexAddress->state)),
-            'country'  => $vtexAddress->country,
+            'city' => ucwords(mb_strtolower($vtexAddress->city)),
+            'state' => ucwords(mb_strtolower($vtexAddress->state)),
+            'country' => $vtexAddress->country,
         ];
 
         return $address;
