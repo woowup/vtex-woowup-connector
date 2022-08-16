12ff0883fcb73d877b04165eaf766eccc1d22970
FranHWU <84875167+FranHWU@users.noreply.github.com>
2022-08-16 19:39:33 -0300

    Revert "WI-1196 agrego método para locationcleaner"

--- src/Stages/AbstractStage.php
+++ src/Stages/AbstractStage.php
@@ -12,23 +12,4 @@ abstract class AbstractStage implements StageInterface
 	{
 		$this->connector = $connector;
 	}
-
-    public function cleanLocation($locationCleaner, $customer)
-    {
-        $state = (isset($customer['state']) && !empty($customer['state'])) ? $customer['state'] : '';
-        $department = (isset($customer['department']) && !empty($customer['department'])) ? $customer['department'] : '';
-        $city = (isset($customer['city']) && !empty($customer['city'])) ? $customer['city'] : '';
-        $locationClean = $locationCleaner->getCleanLocation($state, $department, $city);
-        foreach ($locationClean as $key => $value) {
-            if (empty($value)) {
-                if (isset($customer[$key])) {
-                    unset($customer[$key]);
-                }
-            } else {
-                $customer[$key] = $value;
-            }
-        }
-
-        return $customer;
-    }
 }
